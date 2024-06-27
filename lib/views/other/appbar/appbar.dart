0ec4cabe203d5026483bffbddc42aff99f1dfd59
arvandha121 <nauvan121@gmail.com>
2024-06-27 22:38:00 +0700

    tambah copy url template excel

--- lib/views/other/appbar/appbar.dart
+++ lib/views/other/appbar/appbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart'; // Import the services package for clipboard
 import 'package:weddingcheck/app/provider/provider.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -44,15 +45,18 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
                     ),
+                    textAlign: TextAlign.center,
                   ),
                   content: Column(
                     mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Text(
                         "Klik button di bawah untuk mengunduh template excel list tamu",
                         style: TextStyle(
                           fontSize: 16,
                         ),
+                        textAlign: TextAlign.center,
                       ),
                       SizedBox(height: 20),
                       ElevatedButton.icon(
@@ -73,6 +77,8 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
                           _launchUrl();
                         },
                       ),
+                      SizedBox(height: 10), // Add some spacing
+                      CopyUrlButton(url: url),
                     ],
                   ),
                 );
@@ -86,4 +92,61 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class CopyUrlButton extends StatefulWidget {
+  final Uri url;
+
+  CopyUrlButton({required this.url});
+
+  @override
+  _CopyUrlButtonState createState() => _CopyUrlButtonState();
+}
+
+class _CopyUrlButtonState extends State<CopyUrlButton> {
+  bool _isCopied = false;
+
+  void _copyUrlToClipboard(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: widget.url.toString()));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('URL copied to clipboard'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+
+    setState(() {
+      _isCopied = true;
+    });
+
+    Future.delayed(Duration(seconds: 3), () {
+      if (mounted) {
+        setState(() {
+          _isCopied = false;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      icon: Icon(_isCopied ? Icons.check : Icons.copy, color: Colors.white),
+      label: Text(_isCopied ? 'Copied' : 'Copy URL'),
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.deepPurple,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+      ),
+      onPressed: () {
+        _copyUrlToClipboard(context);
+      },
+    );
+  }
 }
